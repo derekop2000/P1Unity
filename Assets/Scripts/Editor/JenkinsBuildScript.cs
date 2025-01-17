971392791dbd7d77294ae0ecb3a07f098792c26f
derekop <derekop2000@gmail.com>
2025-01-17 09:36:26 +0900

    Jenkins AutoBuild

---
+++ Assets/Scripts/Editor/JenkinsBuildScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+
+using UnityEngine;
+
+public class JenkinsBuildScript
+{
+    [MenuItem("Build/Build Project")]
+    public static void PerformBuild()
+    {
+        string[] scenes = { "Assets/Scenes/MainScene.unity" };
+        string path = "Builds/Windows/P1.exe";
+
+        BuildPipeline.BuildPlayer(scenes, path, BuildTarget.StandaloneWindows, BuildOptions.None);
+    }
+}
